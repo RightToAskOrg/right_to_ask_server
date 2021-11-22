916c414966dc68d2de0f42046e59564e86b15272
Andrew Conway <arcgit@greatcactus.org>
2021-11-22 16:48:48 +1100

    Parse VIC csv for MPs

--- right_to_ask_api/src/bin/update_mp_list_of_files.rs
+++ right_to_ask_api/src/bin/update_mp_list_of_files.rs
@@ -3,7 +3,7 @@ use right_to_ask_api::mp::{update_mp_list_of_files, create_mp_list};
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     update_mp_list_of_files().await?;
-    create_mp_list()?;
+    // create_mp_list()?;
     println!("Ran successfully");
     Ok(())
 }
