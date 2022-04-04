e85b41ed64fb353ba5f5107332a4dc51e35fd354
Andrew Conway <arcgit@greatcactus.org>
2022-04-04 13:07:14 +1000

    Preliminary parse of committee html - actually download

--- right_to_ask_api/src/bin/update_upcoming_hearings.rs
+++ right_to_ask_api/src/bin/update_upcoming_hearings.rs
@@ -3,7 +3,7 @@ use right_to_ask_api::parse_upcoming_hearings::{create_hearings_list, update_hea
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     println!("Downloading into data/upcoming_hearings and checking files");
-    // update_hearings_list_of_files().await?;
+    update_hearings_list_of_files().await?;
     println!("Creating data/upcoming_hearings and checking files/hearings.json");
     create_hearings_list().await?;
     println!("Ran successfully");
