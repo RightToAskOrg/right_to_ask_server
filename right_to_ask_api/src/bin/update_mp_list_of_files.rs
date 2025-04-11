5682487fefeed8eab8913750e847a94353689465
Andrew Conway <arcgit@greatcactus.org>
2025-04-11 14:58:11 +1000

    Re-enable downloading (disabled for debugging in previous commit)

--- right_to_ask_api/src/bin/update_mp_list_of_files.rs
+++ right_to_ask_api/src/bin/update_mp_list_of_files.rs
@@ -3,7 +3,7 @@ use right_to_ask_api::mp::{update_mp_list_of_files, create_mp_list};
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     println!("Downloading into MP_Source/ and checking files");
-    //update_mp_list_of_files().await?;
+    update_mp_list_of_files().await?;
     println!("Creating MP_source/MPs.json");
     create_mp_list()?;
     println!("Ran successfully");
