d7841c04137bf4520bdbef006db3bc99d7f886cd
vteague <vanessafromgit@thinkingcybersecurity.com>
2025-07-02 15:07:43 +1000

    Proper regex for stripping quotes.

--- right_to_ask_api/src/parse_util.rs
+++ right_to_ask_api/src/parse_util.rs
@@ -112,19 +112,6 @@ pub fn relative_url(base_url:&str,url:&str) -> anyhow::Result<String> {
     Ok(res.to_string())
 }
 
-/// Strip the outer quotes, either '...' or "...", from a string, if present.
-pub fn strip_quotes(s: &str) -> String {
-    let double_quote_regexp = Regex::new("^\"(?s)(?<M>.?)\"$").unwrap();
-    // let single_quote_regexp = Regex::new("^\'(?s)(?<N>.?)\'$").unwrap();
-    let matched = &double_quote_regexp.captures(s);
-    if let Some(inner) = matched {
-       inner["M"].to_string()
-    } else {
-        s.to_string()
-    }
-    // let msg2 = &single_quote_regexp.captures(msg).unwrap()["N"];
-}
-
 /// extracts as a string a nested json value, by getting each field in sequence.
 /// if strip_quotes is true, it applies strip_quotes before returning the string.
 pub fn extract_nested_json(json:&Value, fields: &Vec<String>, strip_quotes: bool) -> Option<String> {
@@ -138,16 +125,36 @@ pub fn extract_nested_json(json:&Value, fields: &Vec<String>, strip_quotes: bool
 
 }
 
+/// Strip a single pair of outer quotes, either '...' or "...", from a string, if present.
+pub fn strip_quotes(s: &str) -> String {
+    let double_quote_regexp = Regex::new(r#"^"(?s)(.*)"$"#).unwrap();
+    let single_quote_regexp = Regex::new(r#"^'(?s)(.*)'$"#).unwrap();
+    if let Some(inner_d) = &double_quote_regexp.captures(s) {
+       inner_d[1].to_string()
+    } else if let Some(inner_s) = &single_quote_regexp.captures(s) {
+       inner_s[1].to_string()
+    } else {
+        s.to_string()
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_strip_quotes1() {
-        assert_eq!("Hi", strip_quotes("\"Hi\""));
-        assert_eq!("Hi", strip_quotes("\'Hi\'"));
-        assert_eq!("Hi", strip_quotes("Hi"));
-        assert_eq!("\'Hi", strip_quotes("\'Hi"));
-        assert_eq!("Hi\"", strip_quotes("Hi\""));
+        assert_eq!("Hi", strip_quotes("\"Hi\""));         // Strip matched double quotes
+        assert_eq!("Hi", strip_quotes("\'Hi\'"));         // Strip matched single quotes
+        assert_eq!("Hi", strip_quotes("Hi"));             // Keep things with no quotes
+        assert_eq!("\'Hi", strip_quotes("\'Hi"));         // Don't strip unmatched quotes
+        assert_eq!("Hi\"", strip_quotes("Hi\""));         // "
+        assert_eq!("H\"i\"", strip_quotes("H\"i\""));     // Don't strip quotes that aren't at the ends
+        assert_eq!("\"Hi\'", strip_quotes("\"Hi\'"));     // Don't strip quotes that don't match
+        assert_eq!("\"Hi\"", strip_quotes("\"\"Hi\"\"")); // Only stip the outer layer
+        assert_eq!("\'Hi\'", strip_quotes("\"\'Hi\'\"")); // "
+        assert_eq!("Hi\n", strip_quotes("\"Hi\n\""));     // Include newlines
+        assert_eq!("Hi\n", strip_quotes("\'Hi\n\'"));     // "
     }
 }
